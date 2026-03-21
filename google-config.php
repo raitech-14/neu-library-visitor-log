181fe89b33db89e0fc742594f66cb2730c5d309a
raikereu <raiza.visaya@neu.edu.ph>
2026-03-22 04:07:26 +0800

    Update google-config.php

--- google-config.php
+++ google-config.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 require_once 'vendor/autoload.php';
 
@@ -8,16 +7,3 @@ $client->addScope("email");
 $client->addScope("profile");
 $client->setRedirectUri('http://localhost/VisitorLoginSystem/google-callback.php');
 ?>
-=======
-<?php
-require_once 'vendor/autoload.php';
-
-$client = new Google_Client();
-
-$client->setClientId(getenv('GOOGLE_CLIENT_ID'));
-$client->setClientSecret(getenv('GOOGLE_CLIENT_SECRET'));
-$client->setRedirectUri(getenv('GOOGLE_REDIRECT_URI'));
-
-$client->addScope("email");
-$client->addScope("profile");
->>>>>>> 3dd07543472475057b3aa40fa4d6caf9c87cc434
