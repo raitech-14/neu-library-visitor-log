4e37fbfbef58a7f9074fe16f7ec840dd448802f2
Raiza Visaya <raizavisaya@gmail.com>
2026-03-22 03:00:53 +0800

    Remove leftover merge conflict markers

--- db.php
+++ db.php
@@ -1,26 +1,11 @@
-<<<<<<< HEAD
 <?php
 $host = "localhost";
-$username = "root";
-$password = "";
-$database = "neu_library_log";
+$user = "root";
+$pass = "";
+$dbname = "visitor_log";
 
-$conn = new mysqli($host, $username, $password, $database);
+$conn = new mysqli($host, $user, $pass, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-=======
-<?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "neu_library_log";
-
-$conn = new mysqli($host, $username, $password, $database);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
->>>>>>> 3dd07543472475057b3aa40fa4d6caf9c87cc434
-?>
