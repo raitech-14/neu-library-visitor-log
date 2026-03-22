df617eac78942bb554e18b140d0cab9f28924c22
raikereu <raiza.visaya@neu.edu.ph>
2026-03-23 06:52:08 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,11 @@
 <?php
 session_start();
+include 'functions.php'; 
+
+if (isset($_SESSION['admin'])) {
+    logActivity($pdo, "Admin logged out", $_SESSION['admin']);
+}
+
 session_destroy();
 header("Location: index.php");
 exit();
